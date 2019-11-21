81dc98ac978e8bdc905d6b13b7f81429d3302b04
Fatemeh Pahlevan Aghababa <reyhaneh.pahlevan@gmail.com>
2019-11-21 13:59:11 +0330

    Update simplus.js

--- simplus.js
+++ simplus.js
@@ -16,21 +16,26 @@ new (function() {
     // Functions for block with type 'w' will get a callback function as the 
     // final argument. This should be called to indicate that the block can
     // stop waiting.
-    ext.wait_random = function(callback) {
-        wait = Math.random();
-        console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
+    ext.turnoff_led = function(callback) {
+         $.ajax({
+              url: 'http://localhost:8080/turnoff_led',
+              dataType: 'jsonp',
+              success: function( response ) {
+                  // Got the data - parse it and return the temperature
+                  temperature = response;
+                  console.warn(temperature)
+                  callback(temperature);
+              }
+        });
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['w', 'wait for random time', 'wait_random'],
+            ['R', 'Turn off LED', 'turnoff_led'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Random wait extension', descriptor, ext);
+    ScratchExtensions.register('Simplus', descriptor, ext);
 })();
