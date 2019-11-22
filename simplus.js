49610a0894ba67619dc0d1ebee790640147728c6
Fatemeh Pahlevan Aghababa <reyhaneh.pahlevan@gmail.com>
2019-11-22 11:43:14 +0330

    Update simplus.js

--- simplus.js
+++ simplus.js
@@ -1,129 +1,109 @@
-/* Extension demonstrating a blocking command block */
-/* Reyhaneh <reyhanehpahlevan@ce.sharif.edu>, Nov 2019 */
+#pip install bottle
 
-new (function() {
-    var ext = this;
+from bottle import route, run, template,request
 
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
 
-    // Functions for block with type 'w' will get a callback function as the 
-    // final argument. This should be called to indicate that the block can
-    // stop waiting.
-    ext.set_led = function(status,callback) {
-         $.ajax({
-              url: 'http://localhost:8080/set_led?status='+status,
-              dataType: 'jsonp',
-              success: function( response ) {
-                  callback(response);
-              }
-        });
-    };
-    
-    ext.get_proximity = function(number,callback) {
-         $.ajax({
-              url: 'http://localhost:8080/get_proximity?number='+number,
-              dataType: 'jsonp',
-              success: function( response ) {
-                  callback(response);
-              }
-        });
-    };
-    
-        
-    ext.get_color = function(sensor,callback) {
-         $.ajax({
-              url: 'http://localhost:8080/get_color?sensor='+sensor,
-              dataType: 'jsonp',
-              success: function( response ) {
-                  callback(response);
-              }
-        });
-    };
-    
-    ext.get_position = function(position,callback) {
-         $.ajax({
-              url: 'http://localhost:8080/get_position?position='+position,
-              dataType: 'jsonp',
-              success: function( response ) {
-                  callback(response);
-              }
-        });
-    };
-    
-    ext.get_orientation = function(orientation,callback) {
-         $.ajax({
-              url: 'http://localhost:8080/get_orientation?orientation='+orientation,
-              dataType: 'jsonp',
-              success: function( response ) {
-                  callback(response);
-              }
-        });
-    };
-    
-    
-    ext.set_wheels = function(rw,lw, callback) {
-        // Make an AJAX call to the Open Weather Maps API
-        $.ajax({
-              url: 'http://localhost:8080/set_wheels?rw='+rw+'&lw='+lw,
-              dataType: 'jsonp',
-              success: function( response ) {
-                  callback(response);
-              }
-        });
-    };
+@route('/set_wheels')
+def set_wheels():
+	rw=request.GET.get('rw', '').strip()
+	lw=request.GET.get('lw', '').strip()
+	
+	print("set_wheels")
+	return "set_wheels"
+	
 
-	ext.get_distance_victim = function(callback) {
-        $.ajax({
-              url: 'http://localhost:8080/get_distance_victim',
-              dataType: 'jsonp',
-              success: function( response ) {
-		  console.warn('Waiting for ' + response + ' seconds');
-                  callback(response);
-              }
-        });
-    };
-    
-    ext.send_action = function(action,callback) {
-        $.ajax({
-              url: 'http://localhost:8080/send_action?action='+action,
-              dataType: 'jsonp',
-              success: function( response ) {
-                  callback(response);
-              }
-        });
-    };
-    
-    
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            ['R', 'Get Proximity sensor value for %m.proximities', 'get_proximity','1'],
-            ['R', 'Get %m.colorsensors Color sensor value' , 'get_color','center'],
-            ['R', 'Get Position %m.positions' , 'get_position','x'],
-            ['R', 'Get Orientation %m.orientations' , 'get_orientation','Ro'],
-            ['R', 'Get Distance to Victim ','get_distance_victim'],
-            ['w', 'Change LED status to %m.led', 'set_led','off'],
-            ['w', 'Right Wheel %n Left Wheel %n', 'set_wheels', '0','0'],
-			['w', 'Action %m.actions', 'send_action', 'Find Victim'],
+@route('/set_led')
+def set_led():
+	status=request.GET.get('status', '').strip()
+	if status == "off":
+		print("off")
+	elif status == "blue":
+		print("blue")
+	elif status == "green":
+		print("green")
+	elif status == "red":
+		print("red")
 
-        ],
-        menus: {
-        	led: ['off', 'blue', 'green','red'],
-        	colorsensors: ['center', 'right', 'left'],
-        	proximities:['1','2','3','4','5','6','7','8'],
-        	positions:['x','y','z'],
-        	orientations:['Ro','Phi','Theta'],
-        	actions: ['Find Victim', 'Find Checkpoint','Rescue Victim'],
-    },
-    };
+	return "set_led"
 
-    // Register the extension
-    ScratchExtensions.register('Simplus', descriptor, ext);
-})();
+
+@route('/get_proximity')
+def get_proximity():
+	number=request.GET.get('number', '').strip()
+	print(number)
+	value=0
+	return value
+	
+	
+@route('/get_color')
+def get_color():
+	sensor=request.GET.get('sensor', '').strip()
+	color='red'; # blue,green,gray,black
+	if sensor == "center":
+		print("center")
+	elif sensor == "right":
+		print("right")
+	elif sensor == "left":
+		print("left")
+
+	return color
+	
+
+	
+@route('/get_position')
+def get_position():
+	position=request.GET.get('position', '').strip()
+	value=0;
+	if position == "x":
+		print("x")
+	elif position == "y":
+		print("y")
+	elif position == "z":
+		print("z")
+
+	return value
+	
+	
+@route('/get_orientation')
+def get_orientation():
+	orientation=request.GET.get('orientation', '').strip()
+	value=0;
+	if orientation == "Ro":
+		print("Ro")
+	elif orientation == "Phi":
+		print("Phi")
+	elif orientation == "Theta":
+		print("Theta")
+
+	return value
+
+
+@route('/get_distance_victim')
+def get_distance_victim():
+	#calculate min distance to victims
+	res={};
+	res['a']=0;
+	value=0;
+	return res;
+	
+	
+@route('/send_action')
+def send_action():
+	action=request.GET.get('action', '').strip()
+
+	if action == 'Find Victim':
+		print("Find Victim")
+	elif orientation == "Find Checkpoint":
+		print("Find Checkpoint")
+	elif orientation == "Rescue Victim":
+		print("Rescue Victim")
+
+	
+	#calculate min distance to victims
+	value=0;
+	return value;
+ 
+
+
+run(host='localhost', port=8080)
